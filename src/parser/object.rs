93879dcc016982b14e24479c566f64274e5e4e70
vdinovi <vito.dinovi@gmail.com>
2019-08-18 01:53:33 -0700

    added better display for objects

--- src/parser/object.rs
+++ src/parser/object.rs
@@ -5,13 +5,19 @@ pub struct Object {
     pub map: HashMap<String, Value>,
 }
 
-impl fmt::Debug for Object {
+impl fmt::Display for Object {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Object: {{\n").expect("cant write debug string");
+        write!(f, "{{\n").expect("failed to write object");
         for (key, value) in &self.map {
-            write!(f, "  {}: {:?}\n", key, value).expect("cant write debug string");
+            write!(f, "  {}: {}\n", key, value).expect("failed to write object");
         };
         write!(f, "}}\n")
+    }
+}
+
+impl fmt::Debug for Object {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self)
     }
 }
 
@@ -22,11 +28,12 @@ pub enum Value {
     Array(Vec<Value>)
 }
 
-impl fmt::Debug for Value {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+impl fmt::Display for Value {
+    fn fmt(&self, f : &mut fmt::Formatter) -> fmt::Result {
         let value = match self {
-            Value::Object(_) => "Object".to_string(),
-            Value::Array(_) => "Array".to_string(),
+            Value::Object(object) => object.to_string(),
+            Value::Array(array) => array.iter()
+                                     .fold(String::new(), |res, value| format!("{}, {}", res, value.to_string())),
             Value::String(string) => string.to_string(),
             Value::Number(number) => number.to_string()
         };
